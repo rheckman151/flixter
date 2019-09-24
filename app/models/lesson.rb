b3545e1a005d004a47fa831a5006d78180b0e9a6
rheckman151 <frisbeeryan151@gmail.com>
2019-09-24 04:06:21 +0000

    console work arrange

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,7 +3,7 @@ class Lesson < ApplicationRecord
   mount_uploader :video, VideoUploader
 
   include RankedModel
-  ranks :row_order
+  ranks :row_order, with_same: :section_id
   
   def next_lesson
     lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
