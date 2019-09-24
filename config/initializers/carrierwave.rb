2666c62dfe5ec8ca61b656545e3d2c0a8f6870f8
rheckman151 <frisbeeryan151@gmail.com>
2019-09-24 22:54:20 +0000

    commented out in development.rb, changed - to _ initializer, scss background deleted, _test added to storage.yml

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,12 @@
+if Rails.env.development?
+    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
+
 CarrierWave.configure do |config|
     config.storage    = :aws
     config.aws_bucket = ENV["AWS_BUCKET"]
-    config.aws_acl    = "public-read"
+    config.aws_acl    = "public_read"
   
     config.aws_credentials = {
         access_key_id:     ENV["AWS_ACCESS_KEY"],
--- config/spring.rb
+++ config/spring.rb
@@ -1,6 +1,12 @@
+
 %w[
+
   .ruby-version
+
   .rbenv-vars
+
   tmp/restart.txt
+
   tmp/caching-dev.txt
+
 ].each { |path| Spring.watch(path) }
