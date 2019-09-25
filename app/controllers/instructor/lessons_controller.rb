e3c46b159179c143b2cfa6930f25e294532b29a1
rheckman151 <frisbeeryan151@gmail.com>
2019-09-25 00:56:22 +0000

    instructor controller 22-26

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -19,9 +19,15 @@ class Instructor::LessonsController < ApplicationController
     @current_lesson ||= Lesson.find(params[:id])
   end
 
-  def require_authorized_for_current_section
+  def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
